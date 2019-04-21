8bb9ef4112143e2a9ece3ec18e17d4051478fe55
Amarnath Vadivelu <Amarnath Vadivelu@192.168.1.4>
2019-04-21 11:47:17 +0530

    Testing a commit_1st try

---
+++ javjenkins/src/javjenkins/Helloworld.java
@@ -0,0 +1,11 @@
+package javjenkins;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Trying for handshake Jenkins and GIT");
+	}
+
+}
